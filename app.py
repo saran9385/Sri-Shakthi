6fecf8551e71a834fb98be621a4e419d7c8b3829
saran9385 <saran97916@gmail.com>
2025-09-18 16:43:21 +0530

    Update 3 Google Anlayics

--- app.py
+++ app.py
@@ -240,4 +240,5 @@ def update_price():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
