2b5ef48062edde5cf82eda634aaa5b9251b21081
saran9385 <saran97916@gmail.com>
2025-09-18 15:37:35 +0530

    Update 1 Google Anlayics

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True) 
-db.init_app(app)
+# db.init_app(app)
 
 
 # Helper function to check file extension
