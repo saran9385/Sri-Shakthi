f2a035013910b756c449ae2b0b805d5a8aa590b2
saran9385 <saran97916@gmail.com>
2025-09-18 16:25:35 +0530

    Update 3 Google Anlayics

--- app.py
+++ app.py
@@ -233,7 +233,10 @@ def update_price():
     
     return jsonify({'success': 'Price updated successfully'}), 200
 
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
